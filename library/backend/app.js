00b3ba5b31bd59119eb871c2d02156bb34eb5cf9
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2021-04-08 14:27:18 -0300

    feat: add seventh exercise

--- library/backend/app.js
+++ library/backend/app.js
@@ -1,40 +1,34 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const Book = require('./models/Book')
 
-const books = [
-  {
-    bookId: 1,
-    title: 'The Man in the High Castle',
-    author: 'Philip K Dick',
-    pages: 237
-  },
-  {
-    bookId: 2,
-    title: 'Neuromancer',
-    author: 'William Gibson',
-    pages: 320
-  }
-]
+mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING)
+  .then(() => console.log('Mongoose connected'))
+  .catch(() => console.error('Mongoose con\'t be connected'))
 
 app.use(bodyParser.json())
 app.use(cors())
 
 app.post('/api/books', (req, res) => {
-  const book = {
-    bookId: books[books.length - 1].bookId + 1,
+  const book = new Book({
     ...req.body
-  }
-  books.push(book)
+  })
+  book.save()
   res
     .status(201)
     .json(book)
 })
 
 app.get('/api/books', (_, res) => {
-  res
-    .status(200)
-    .json(books)
+  Book
+    .find()
+    .then(doc => {
+      res
+        .status(200)
+        .json(doc)
+    })
 })
 
 module.exports = app
